9d718f8a82893b3e464e2c586d41210d0a372c74
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-13 19:09:38 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 def start_game(player1, player2)
-  puts "Hello #{player1} & #{playar2}"
+  greeting = "Hello #{player1} & #{playar2}"
 end
 
 def play_game(player2)
-  puts "#{player1} is better than #{player2}"
+  greeting = "#{player1} is better than #{player2}"
 end
