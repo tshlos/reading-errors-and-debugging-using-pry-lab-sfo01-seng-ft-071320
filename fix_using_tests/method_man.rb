81c1825f64b2e562e3e74dfb74f004cd62f98cb6
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-13 19:50:22 +0000

    Done.

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,10 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  greeting = "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2)
-  "#{player1} is better than #{player2}"
+def play_game(player1, player2)
+  greeting = "#{player1} is better than #{player2}"
+
 end
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,12 @@
 # don't forget to add: require 'pry'
 
+
 def snake_it_up(string)
+
   if string[0] == "s"
-    10 * "s" + string
+    return ("s" * 10) + string
   else
-    string
+    return string
   end
 end
+
