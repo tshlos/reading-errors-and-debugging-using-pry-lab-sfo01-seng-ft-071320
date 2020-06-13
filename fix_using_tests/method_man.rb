eaa8a4714ba6df9c9f6ceeb913f5ea2a9510c1c1
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-13 19:13:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,7 +4,7 @@ def start_game(player1, player2)
   greeting = "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2)
+def play_game(player1, player2)
   greeting = "#{player1} is better than #{player2}"
-  return greeting
+
 end
